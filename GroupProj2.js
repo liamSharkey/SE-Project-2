36a188325de642e4f8a916accf58b4b0dee840b3
ebodnar1 <71098182+ebodnar1@users.noreply.github.com>
2020-10-18 17:26:42 -0600

    Update GroupProj2.js
    Added comments

--- GroupProj2.js
+++ GroupProj2.js
@@ -1,9 +1,17 @@
+//Creating a new prototype for Point objects
 let coordProtoype = {
     x: 0,
     y: 0,
     z: 0
 };
 
+/*
+Point constructor 
+Takes 3 values (x,y,z coordinates)
+Creates an object based on coordPrototype to compare the 3 parameter values to
+Function calcDistance takes this object and takes the difference between the x, y, and z coordinates of both objects
+Then it adds up the squares of those results and square roots the sum
+*/
 function Point(x, y, z){
     this.x = x;
     this.y = y;
@@ -27,8 +35,10 @@ function Point(x, y, z){
     }
 }
 
+//Create two objects of type point using the constructor function
 let p1 = new Point(1,2,3);
 let p2 = new Point(3,-1,3);
 
+//Compare the difference in location between the two points in 3D space and print the resulting distance
 let result = p1.calcDistance(p2);
 console.log(result);
